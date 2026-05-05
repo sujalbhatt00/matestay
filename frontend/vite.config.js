343c77abd008534f9a968d1443322d751b9aff5e
Sujal Bhatt <sujalbhatt500@gmail.com>
2026-05-06 01:16:20 +0530

    changed vite config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,20 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()],
-    resolve: {
+    react()
+  ],
+  resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
     },
   },
 })
